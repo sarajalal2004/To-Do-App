8e1e2bb80c831a833af6229bc53dda087814c307
sarajalal2004 <sarauobeducation@gmail.com>
2026-01-02 17:39:44 +0300

    Step 1 - COMPLETED

---
+++ src/main/java/com/example/toDoApp/controller/Hello.java
@@ -0,0 +1,16 @@
+package com.example.toDoApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api")
+public class Hello {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+
+}
